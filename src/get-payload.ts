725dab9cec0e2cf447a80ad01824d70c3034428f
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2024-03-20 23:45:06 -0300

    :sparkles: add email verified and views to show in error token

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,9 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
+import nodemailer from "nodemailer";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -20,7 +31,9 @@ interface IArgsProps {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: IArgsProps = {}): Promise<Payload> => {
+export const getPayloadClient = async ({
+  initOptions,
+}: IArgsProps = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing in environment config");
   }
@@ -31,6 +44,11 @@ export const getPayloadClient = async ({ initOptions }: IArgsProps = {}): Promis
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@cmatteo.dev",
+        fromName: "DigitalHippo",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
