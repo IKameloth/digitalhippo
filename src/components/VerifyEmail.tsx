6a8273933f4eb5759feac00beb756909c7e505fd
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2024-03-21 16:12:22 -0300

    :sparkles: add create new account ready

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
-import { XCircle } from "lucide-react";
+import { Loader2, XCircle } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
-interface IVerifyProps {
+interface IVerifyEmailProps {
   token: string;
 }
 
-const VerifyEmail = ({ token }: IVerifyProps) => {
+const VerifyEmail = ({ token }: IVerifyEmailProps) => {
   const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
     token,
   });
@@ -23,7 +26,36 @@ const VerifyEmail = ({ token }: IVerifyProps) => {
       </div>
     );
   }
-  return <div></div>;
+
+  if (data?.success) {
+    return (
+      <div className="flex flex-col h-full items-center justify-center">
+        <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+          <Image src="/hippo-email-sent.png" alt="the email was sent" fill />
+        </div>
+
+        <h3 className="font-semibold text-2xl">You&apos;re all set!</h3>
+        <p className="text-muted-foreground text-center mt-1">
+          Thank you for verifying your email
+        </p>
+        <Link className={buttonVariants({ className: "mt-4" })} href="/sign-in">
+          Sign in
+        </Link>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="animate-spin h-8 w-8 text-zinc-300" />
+        <h3 className="font-semibold text-xl">Verifying...</h3>
+        <p className="text-muted-foreground text-sm">
+          This won&apos;t take long.
+        </p>
+      </div>
+    );
+  }
 };
 
 export default VerifyEmail;
